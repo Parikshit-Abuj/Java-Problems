66f08be55936a10c2132137b860c1db9ed040656
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-09 20:52:23 +0530

    Create AutomorphicNumber.java

---
+++ AutomorphicNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AutomorphicNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int square = num * num;
+        int temp = num;
+
+        while (temp > 0) {
+            if (temp % 10 != square % 10) {
+                System.out.println(num + " is not an Automorphic number");
+                return;
+            }
+            temp = temp / 10;
+            square = square / 10;
+        }
+
+        System.out.println(num + " is an Automorphic number");
+    }
+}
