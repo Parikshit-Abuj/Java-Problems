36174655af8ddad7f55d30d23c624fbd54ef8e28
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-14 17:13:21 +0530

    Create AnagramCheck.java

---
+++ String Maniulation, Immutability, Regex/AnagramCheck.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
+
+    static boolean areAnagrams(String s1, String s2) {
+        // Remove spaces and convert to lowercase
+        s1 = s1.replaceAll("\\s", "").toLowerCase();
+        s2 = s2.replaceAll("\\s", "").toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
